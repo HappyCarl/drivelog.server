bbcaf9bef56a546a34c3b5dd6ab4bb9f340ce865
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-04-10 13:22:19 +0200

    add env vars to travis and other awesome things

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ DriveLog
 
 Please use git flow. It works. Release-branch is "master". For Development, create a feature/ branch originating from develop. It's great.
 
-Put all the config keys in drivelog.server-war/src/main/webapp/WEB-INF/keys.properties
--- drivelog.server-war/src/main/java/de/happycarl/drivelog/server/Constants.java
+++ drivelog.server-war/src/main/java/de/happycarl/drivelog/server/Constants.java
@@ -1,9 +1,9 @@
 package de.happycarl.drivelog.server;
 
 public class Constants {
-	public static final String WEB_CLIENT_ID = "replace this with your web client application ID";
-	public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
-	public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+	public static final String WEB_CLIENT_ID = "REPLACE_WEB_CLIENT_ID";
+	public static final String ANDROID_CLIENT_ID = "REPLACE_ANDROID_CLIENT_ID";
+	public static final String IOS_CLIENT_ID = "REPLACE_IOS_CLIENT_ID";
 	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 
 	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";

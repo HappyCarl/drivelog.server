7247f7ffa27e710eb0bc2c5f249ebe9b0b1b51de
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-06-11 14:19:25 +0200

    implemented get current userdata endpoint

--- drivelog.server-war/src/main/java/de/happycarl/drivelog/server/DriveLogEndpoints.java
+++ drivelog.server-war/src/main/java/de/happycarl/drivelog/server/DriveLogEndpoints.java
@@ -1,8 +1,9 @@
 package de.happycarl.drivelog.server;
 
-import javax.inject.Named;
-
 import com.google.api.server.spi.config.Api;
+import com.google.appengine.api.users.User;
+
+import de.happycarl.drivelog.server.models.UserData;
 
 import static de.happycarl.drivelog.server.OfyService.ofy;
 
@@ -11,4 +12,19 @@ import static de.happycarl.drivelog.server.OfyService.ofy;
 		Constants.IOS_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, audiences = { Constants.ANDROID_AUDIENCE })
 public class DriveLogEndpoints {
 
+	public UserData getCurrentUserData(User user) {
+		return getOrCreateUserData(user);
+	}
+
+	private static UserData getOrCreateUserData(User user) {
+		UserData data = ofy().load().type(UserData.class).id(user.getEmail())
+				.now();
+
+		if (data == null) {
+			data = new UserData(user);
+		}
+		ofy().save().entity(data).now();
+
+		return data;
+	}
 }

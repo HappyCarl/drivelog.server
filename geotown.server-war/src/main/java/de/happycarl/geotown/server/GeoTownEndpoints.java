20a7213e1bdbcfbc8d2fa211b34c9a393e2d4823
Jan-Henrik <jh.bruhn@me.com>
2014-06-12 17:09:51 +0200

    Added more api calls.

--- geotown.server-war/src/main/java/de/happycarl/geotown/server/GeoTownEndpoints.java
+++ geotown.server-war/src/main/java/de/happycarl/geotown/server/GeoTownEndpoints.java
@@ -1,12 +1,15 @@
 package de.happycarl.geotown.server;
 
 import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.ForbiddenException;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
 
 import com.googlecode.objectify.Ref;
 import de.happycarl.geotown.server.models.Route;
 import de.happycarl.geotown.server.models.UserData;
+import de.happycarl.geotown.server.models.Waypoint;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +23,46 @@ public class GeoTownEndpoints {
 		return getOrCreateUserData(user);
 	}
 
-    public List<Route> getCurrentUsersRoutes(User user) throws UnauthorizedException {
+    public List<Route> getMyRoutes(User user) throws UnauthorizedException {
         UserData userData = getOrCreateUserData(user);
 
         return userData.getRoutes();
+    }
+
+    public Route createRoute(@Named("name") String name, @Named("latitude") double latitude, @Named("longitude") double longitude, User user) throws UnauthorizedException {
+        UserData userData = getOrCreateUserData(user);
+
+        Route route = new Route(userData, name);
+
+        route.setLatitude(latitude);
+        route.setLongitude(longitude);
+
+        OfyService.ofy().save().entity(route).now();
+
+        return route;
+    }
+
+    public Route addWaypoint(@Named("routeId") long routeId, @Named("lat") double latitude,
+                             @Named("lon") double longitude, @Named("question") String question,
+                             @Named("answers") List<String> answers, User user) throws UnauthorizedException, ForbiddenException {
+        UserData userData = getOrCreateUserData(user);
+
+        Route route = OfyService.ofy().load().type(Route.class).id(routeId).safe();
+
+        if(!route.getOwner().getEmail().equals(userData.getEmail()))
+            throw new ForbiddenException("You're not allowed to edit this route.");
+
+
+        Waypoint w = new Waypoint(route, latitude, longitude);
+        w.setQuestion(question);
+        w.getAnswers().addAll(answers);
+
+        route.getWaypoints().add(w);
+
+        OfyService.ofy().save().entities(w, route).now();
+
+
+        return route;
     }
 
 	private static UserData getOrCreateUserData(User user) throws UnauthorizedException {

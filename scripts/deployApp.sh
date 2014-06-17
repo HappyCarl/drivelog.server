3113f8b6f763d7cad61a1b628fefe93028fa0d30
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-06-17 13:27:17 +0200

    Added support for beta deployment.

--- scripts/deployApp.sh
+++ scripts/deployApp.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -ev
+
+APP_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
+
+ARGS=""
+if [[ $APP_VERSION == *SNAPSHOT* ]]
+then
+    ARGS="-Dversion=beta"
+fi
+
 cd geotown.server-ear/
-echo -e "$GOOGLE_PASSWORD" | mvn appengine:update
+echo -e "$GOOGLE_PASSWORD" | mvn appengine:update $ARGS

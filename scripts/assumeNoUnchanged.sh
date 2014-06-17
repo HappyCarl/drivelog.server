c6bc808ed715178269cdde79e65b0fa96e127202
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-06-17 12:51:48 +0200

    Added some usefull files.

---
+++ scripts/assumeNoUnchanged.sh
@@ -0,0 +1,2 @@
+git update-index --no-assume-unchanged  geotown.server-war/src/main/coffee/constants.coffee                     
+git update-index --no-assume-unchanged  geotown.server-war/src/main/java/de/happycarl/geotown/server/Constants.java

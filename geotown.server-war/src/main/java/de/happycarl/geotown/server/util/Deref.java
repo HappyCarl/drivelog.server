c46f7fc1c710c185e9f0e2f1270fcd56315ea510
Jan-Henrik <jh.bruhn@me.com>
2014-06-18 15:36:22 +0200

    Fixed illegal code.

--- geotown.server-war/src/main/java/de/happycarl/geotown/server/util/Deref.java
+++ geotown.server-war/src/main/java/de/happycarl/geotown/server/util/Deref.java
@@ -1,30 +1,24 @@
 package de.happycarl.geotown.server.util;
 
-import com.google.appengine.repackaged.com.google.common.base.Function;
-import com.google.appengine.repackaged.com.google.common.collect.Lists;
 import com.googlecode.objectify.Ref;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by jhbruhn on 12.06.14.
  */
 public class Deref {
-    public static class Func<T> implements Function<Ref<T>, T> {
-        public static Func<Object> INSTANCE = new Func<Object>();
 
-		public T apply(Ref<T> ref) {
-			return deref(ref);
-		}
-
-    }
 
     public static <T> T deref(Ref<T> ref) {
         return ref == null ? null : ref.get();
     }
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
     public static <T> List<T> deref(List<Ref<T>> reflist) {
-        return Lists.transform(reflist, (Func) Func.INSTANCE);
+        List<T> n = new ArrayList<T>(reflist.size());
+        for(Ref<T> ref : reflist)
+            n.add(deref(ref));
+        return n;
     }
 }

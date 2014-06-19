a68bfb570cfba9bd6f7e09a9e5d46d632123be06
Jan-Henrik <jh.bruhn@me.com>
2014-06-19 21:31:12 +0200

    Did some code formatting.

--- geotown.server-war/src/main/java/de/happycarl/geotown/server/models/Route.java
+++ geotown.server-war/src/main/java/de/happycarl/geotown/server/models/Route.java
@@ -1,14 +1,16 @@
 package de.happycarl.geotown.server.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.api.server.spi.config.AnnotationBoolean;
 import com.google.api.server.spi.config.ApiResourceProperty;
 import com.googlecode.objectify.Ref;
-import com.googlecode.objectify.annotation.*;
-
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 import de.happycarl.geotown.server.util.Deref;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jhbruhn on 12.06.14.
@@ -17,69 +19,69 @@ import de.happycarl.geotown.server.util.Deref;
 @Cache
 @Index
 public class Route {
-	@Id
-	Long id;
+    @Id
+    Long id;
 
-	Ref<UserData> owner;
+    Ref<UserData> owner;
 
-	String name;
+    String name;
 
-	double latitude;
-	double longitude;
+    double latitude;
+    double longitude;
 
     @ApiResourceProperty(ignored = AnnotationBoolean.TRUE)
     List<Ref<Waypoint>> waypoints = new ArrayList<Ref<Waypoint>>();
 
-	public Route(UserData owner, String name) {
-		this.owner = Ref.create(owner);
-		this.name = name;
-	}
+    public Route(UserData owner, String name) {
+        this.owner = Ref.create(owner);
+        this.name = name;
+    }
 
-	private Route() {
-	}
+    private Route() {
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public UserData getOwner() {
-		return Deref.deref(owner);
-	}
+    public UserData getOwner() {
+        return Deref.deref(owner);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<Waypoint> getWaypoints() {
-		return Deref.deref(waypoints);
-	}
+    public List<Waypoint> getWaypoints() {
+        return Deref.deref(waypoints);
+    }
 
-	public double getLatitude() {
-		return latitude;
-	}
+    public double getLatitude() {
+        return latitude;
+    }
 
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
 
-	public double getLongitude() {
-		return longitude;
-	}
+    public double getLongitude() {
+        return longitude;
+    }
 
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 
     public void addWaypoint(Waypoint wP) {
         this.waypoints.add(Ref.create(wP));
     }
 
-	public boolean equals(Route that) {
-		return this.getId()== (that.getId());
-	}
+    public boolean equals(Route that) {
+        return this.getId() == (that.getId());
+    }
 
 }

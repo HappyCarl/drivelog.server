b7c13ce431aa6acf95578e7ed69d3c73545ef969
Jan-Henrik <jh.bruhn@me.com>
2014-06-19 21:31:05 +0200

    Implemented Usernames.

--- geotown.server-war/src/main/java/de/happycarl/geotown/server/GeoTownEndpoints.java
+++ geotown.server-war/src/main/java/de/happycarl/geotown/server/GeoTownEndpoints.java
@@ -1,66 +1,77 @@
 package de.happycarl.geotown.server;
 
-import java.util.List;
-
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.Named;
 import com.google.api.server.spi.response.ForbiddenException;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
-
 import de.happycarl.geotown.server.models.Route;
 import de.happycarl.geotown.server.models.UserData;
 import de.happycarl.geotown.server.models.Waypoint;
 
-@Api(name = "geotown", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = {
-		Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID,
-		Constants.IOS_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, audiences = { Constants.ANDROID_AUDIENCE })
+import java.util.List;
+
+@Api(name = "geotown", version = "v1", scopes = {Constants.EMAIL_SCOPE}, clientIds = {
+        Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID,
+        Constants.IOS_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID}, audiences = {Constants.ANDROID_AUDIENCE})
 public class GeoTownEndpoints {
 
-    @ApiMethod(name="userdata.get", path="userdata")
-	public UserData getCurrentUserData(User user) throws UnauthorizedException {
-		return getOrCreateUserData(user);
-	}
+    @ApiMethod(name = "userdata.get", path = "userdata")
+    public UserData getCurrentUserData(User user) throws UnauthorizedException {
+        return getOrCreateUserData(user);
+    }
 
-    @ApiMethod(name="routes.listMine", path="routes")
-	public List<Route> listMyRoutes(User user) throws UnauthorizedException {
-		UserData userData = getOrCreateUserData(user);
+    @ApiMethod(name = "userdata.setUsername", path = "userdata.username")
+    public UserData setUsername(@Named("username") String username, User user) throws UnauthorizedException {
+        UserData userData = getOrCreateUserData(user);
 
-		return OfyService.ofy().load().type(Route.class).filter("owner", userData).list();
-	}
+        if (username.length() > 0)
+            userData.setUsername(username);
 
-    @ApiMethod(name="routes.insert", path="routes")
-	public Route createRoute(@Named("name") String name,
-			@Named("latitude") double latitude,
-			@Named("longitude") double longitude, User user)
-			throws UnauthorizedException {
-		UserData userData = getOrCreateUserData(user);
+        OfyService.ofy().save().entity(userData).now();
 
-		Route route = new Route(userData, name);
+        return userData;
+    }
 
-		route.setLatitude(latitude);
-		route.setLongitude(longitude);
+    @ApiMethod(name = "routes.listMine", path = "routes")
+    public List<Route> listMyRoutes(User user) throws UnauthorizedException {
+        UserData userData = getOrCreateUserData(user);
 
-		OfyService.ofy().save().entities(route).now();
+        return OfyService.ofy().load().type(Route.class).filter("owner", userData).list();
+    }
+
+    @ApiMethod(name = "routes.insert", path = "routes")
+    public Route createRoute(@Named("name") String name,
+                             @Named("latitude") double latitude,
+                             @Named("longitude") double longitude, User user)
+            throws UnauthorizedException {
+        UserData userData = getOrCreateUserData(user);
+
+        Route route = new Route(userData, name);
+
+        route.setLatitude(latitude);
+        route.setLongitude(longitude);
+
+        OfyService.ofy().save().entities(route).now();
         userData.addRoute(route);
         OfyService.ofy().save().entities(userData).now();
 
         return route;
-	}
+    }
 
-    @ApiMethod(name="routes.get", path="routes.get")
+    @ApiMethod(name = "routes.get", path = "routes.get")
     public Route getRoute(@Named("routeId") Long routeId, User user) {
         return OfyService.ofy().load().type(Route.class).id(routeId)
                 .safe();
     }
 
-    @ApiMethod(name="routes.delete", path="routes")
+    @ApiMethod(name = "routes.delete", path = "routes")
     public void deleteRoute(@Named("routeId") Long routeId, User user) throws ForbiddenException, UnauthorizedException {
         UserData userData = getOrCreateUserData(user);
         Route route = OfyService.ofy().load().type(Route.class).id(routeId).safe();
 
-        if(!route.getOwner().equals(userData))
+        if (!route.getOwner().equals(userData))
             throw new ForbiddenException(
                     "You're not allowed to delete this Route.");
 
@@ -70,66 +81,66 @@ public class GeoTownEndpoints {
         OfyService.ofy().save().entities(userData).now();
     }
 
-    @ApiMethod(name="waypoints.list", path="waypoints")
+    @ApiMethod(name = "waypoints.list", path = "waypoints")
     public List<Waypoint> listWaypoints(@Named("routeId") Long routeId, User user) {
         Route r = OfyService.ofy().load().type(Route.class).id(routeId).safe();
         return OfyService.ofy().load().type(Waypoint.class).filter("route", r).list();
     }
 
-    @ApiMethod(name="waypoints.insert", path="waypoints")
-	public Route createWaypoint(@Named("routeId") Long routeId,
-			@Named("latitude") double latitude, @Named("longitude") double longitude,
-			@Named("question") String question,
-			@Named("answers") List<String> answers, User user)
-			throws UnauthorizedException, ForbiddenException {
-		UserData userData = getOrCreateUserData(user);
+    @ApiMethod(name = "waypoints.insert", path = "waypoints")
+    public Route createWaypoint(@Named("routeId") Long routeId,
+                                @Named("latitude") double latitude, @Named("longitude") double longitude,
+                                @Named("question") String question,
+                                @Named("answers") List<String> answers, User user)
+            throws UnauthorizedException, ForbiddenException {
+        UserData userData = getOrCreateUserData(user);
 
-		Route route = OfyService.ofy().load().type(Route.class).id(routeId)
-				.safe();
+        Route route = OfyService.ofy().load().type(Route.class).id(routeId)
+                .safe();
 
-		if (!route.getOwner().equals(userData))
-			throw new ForbiddenException(
-					"You're not allowed to edit this route.");
+        if (!route.getOwner().equals(userData))
+            throw new ForbiddenException(
+                    "You're not allowed to edit this route.");
 
-		Waypoint w = new Waypoint(route, latitude, longitude);
-		w.setQuestion(question);
-		w.getAnswers().addAll(answers);
+        Waypoint w = new Waypoint(route, latitude, longitude);
+        w.setQuestion(question);
+        w.getAnswers().addAll(answers);
 
         OfyService.ofy().save().entities(w).now();
 
-		route.addWaypoint(w);
+        route.addWaypoint(w);
 
         OfyService.ofy().save().entities(route).now();
 
-		return route;
-	}
+        return route;
+    }
 
-    @ApiMethod(name="waypoints.delete", path="waypoints")
+    @ApiMethod(name = "waypoints.delete", path = "waypoints")
     public void deleteWaypoint(@Named("waypointId") Long waypointId, User user) throws ForbiddenException, UnauthorizedException {
         UserData userData = getOrCreateUserData(user);
         Waypoint waypoint = OfyService.ofy().load().type(Waypoint.class).id(waypointId).safe();
 
-        if(!waypoint.getRoute().getOwner().equals(userData))
+        if (!waypoint.getRoute().getOwner().equals(userData))
             throw new ForbiddenException(
                     "You're not allowed to delete this Waypoint.");
 
         OfyService.ofy().delete().entity(waypoint).now();
     }
 
-	private static UserData getOrCreateUserData(User user)
-			throws UnauthorizedException {
-		if (user == null) {
-			throw new UnauthorizedException("Authorization required");
-		}
+    private static UserData getOrCreateUserData(User user)
+            throws UnauthorizedException {
+        if (user == null) {
+            throw new UnauthorizedException("Authorization required");
+        }
 
-		UserData data = OfyService.ofy().load().type(UserData.class)
-				.id(user.getEmail()).now();
+        UserData data = OfyService.ofy().load().type(UserData.class)
+                .id(user.getEmail()).now();
 
-		if (data == null) {
-			data = new UserData(user);
-			OfyService.ofy().save().entity(data).now();
-		}
+        if (data == null) {
+            data = new UserData(user);
+            OfyService.ofy().save().entity(data).now();
+        }
 
-		return data;
-	}
+        return data;
+    }
 }
--- geotown.server-war/src/main/java/de/happycarl/geotown/server/models/UserData.java
+++ geotown.server-war/src/main/java/de/happycarl/geotown/server/models/UserData.java
@@ -1,8 +1,5 @@
 package de.happycarl.geotown.server.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.api.server.spi.config.AnnotationBoolean;
 import com.google.api.server.spi.config.ApiResourceProperty;
 import com.google.appengine.api.users.User;
@@ -10,34 +7,46 @@ import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
-
 import com.googlecode.objectify.annotation.Index;
 import de.happycarl.geotown.server.util.Deref;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Cache
 @Index
 public class UserData {
-	@Id
-	String email;
+    @Id
+    String email;
 
     @ApiResourceProperty(ignored = AnnotationBoolean.TRUE)
     List<Ref<Route>> routes = new ArrayList<Ref<Route>>();
 
-	public UserData(User user) {
-		this.email = user.getEmail();
-	}
+    String username;
 
-	private UserData() {
-	}
+    public UserData(User user) {
+        this.email = user.getEmail();
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    private UserData() {
+    }
 
-	public List<Route> getRoutes() {
-		return Deref.deref(routes);
-	}
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String n) {
+        this.username = n;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public List<Route> getRoutes() {
+        return Deref.deref(routes);
+    }
 
     public void addRoute(Route route) {
         this.routes.add(Ref.create(route));
@@ -45,17 +54,17 @@ public class UserData {
 
     public void removeRoute(Route route) {
         int index = 0;
-        for(Ref<Route> ref : routes) {
-            if(ref.get().getId() == (route.getId())) {
-             break;
+        for (Ref<Route> ref : routes) {
+            if (ref.get().getId() == (route.getId())) {
+                break;
             }
             index++;
         }
-        if(index > routes.size()) return;
+        if (index > routes.size()) return;
         this.routes.remove(index);
     }
 
-	public boolean equals(UserData that) {
-		return this.getEmail().equals(that.getEmail());
-	}
+    public boolean equals(UserData that) {
+        return this.getEmail().equals(that.getEmail());
+    }
 }
